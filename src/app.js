4991ffc8cda4e7e407a902232b2ea20e2d65f06e
yurik <yurik@NETBET_19>
2019-12-10 21:39:40 -0500

    change app.js

--- src/app.js
+++ src/app.js
@@ -21,11 +21,11 @@ app.get('/api/counter', (req, res) => {
 app.get('/api/users', (req, res) => {
     console.info(`handling USERS request ${new Date().toString()}`);
     const users = [
-        { name: 'Ann', email: 'ann@mail.com' },
-        { name: 'Bill', email: 'bill@mail.com' },
-        { name: 'Lee', email: 'lee@mail.com' },
+        { name: 'Ann', email: 'ann@mail' },
+        { name: 'Bill', email: 'bill@mail' },
+        { name: 'Lee', email: 'lee@mail' },
     ];
-    res.setHeader('Access-Control-Expose-Headers', 'tle-cache-duration-sec');
+    res.setHeader('Access-Control-Expose-Headers', 'my-custom-header');
     res.setHeader('my-custom-header', '123');
     res.send(users);
 })
